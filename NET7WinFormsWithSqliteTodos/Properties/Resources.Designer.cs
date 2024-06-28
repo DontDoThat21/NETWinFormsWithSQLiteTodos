bc601419e134973c1834d337f0f060ece637c16f
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-28 16:37:10 -0400

    Renamed NET .csproj for easier future maintainability reasons.

--- NET7WinFormsWithSqliteTodos/Properties/Resources.Designer.cs
+++ NET7WinFormsWithSqliteTodos/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace NET7WinFormsWithSqliteTodos.Properties {
+namespace NETWinFormsWithSqliteTodos.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace NET7WinFormsWithSqliteTodos.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NET7WinFormsWithSqliteTodos.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NETWinFormsWithSqliteTodos.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
